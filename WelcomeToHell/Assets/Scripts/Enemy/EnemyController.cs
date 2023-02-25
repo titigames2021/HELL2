6721cc3cbdd375b41db58d80678b68099a2351d6
titigames2021 <titigames2021@gmail.com>
2023-02-25 17:03:27 +0100

    H2

---
+++ WelcomeToHell/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float life = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (life <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        life--;
+    }
+}
